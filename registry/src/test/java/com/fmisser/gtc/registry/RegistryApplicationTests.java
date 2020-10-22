724822b8335c61bba5f56066c971597cdedcb210
jy <fmisser@gmail.com>
2020-10-22 11:39:34 +0800

    1. 项目结构整理 2. 添加注册中心配置

---
+++ registry/src/test/java/com/fmisser/gtc/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmisser.gtc.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
