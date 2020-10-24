59c2d9a82673b678a9cbd7ef98827539b1704e96
jy <fmisser@gmail.com>
2020-10-24 17:33:11 +0800

    1. 新增部分sql配置

--- sql/sql_mode_config.sql
+++ sql/sql_mode_config.sql
@@ -1,4 +1,12 @@
+-- modify sql_mode
 show variables like 'sql_mode';
 set session
 sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 show variables like 'sql_mode';
+
+-- modify charset
+show create table `t_role`;
+alter table `t_role` default character set utf8;
+alter table `t_role` change intro intro varchar(255) character set utf8;
+
+
